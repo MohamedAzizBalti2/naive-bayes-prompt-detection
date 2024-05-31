b3a33eeefb1bc0065c604b3e622fd6d003c2ad3d
MohamedAzizBalti2 <ras9iaymen@gmail.com>
2024-05-31 18:20:25 +0100

    Account change

--- app/model/model.py
+++ app/model/model.py
@@ -11,6 +11,8 @@ classes = ['PII', 'benign', 'injection', 'jailbreak']
 with open(f"{BASE_DIR}/pipeline_model_final_version"+".pkl", "rb") as f:
     model = pickle.load(f)
 
+
+
 def predict_pipeline(text):
     predicted_probabilities = model.predict_proba([text])[0]
     class_probabilities = {cls: prob for cls, prob in zip(classes, predicted_probabilities)}
